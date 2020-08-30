96ef37e898f50f5a94b898e595f114ce7ec7f5d2
felipe.bertanha.pra <felipe.bertanha@capitaletecnologia.com.br>
2020-08-30 17:16:36 -0300

    aula 2 - personalized Binding Adapters

---
+++ app/src/main/java/br/com/alura/ceep/ui/databinding/ImageViewBindAdapters.kt
@@ -0,0 +1,15 @@
+package br.com.alura.ceep.ui.databinding
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import br.com.alura.ceep.ui.extensions.carregaImagem
+
+/**
+ * Created by felipebertanha on 30/August/2020
+ */
+
+
+@BindingAdapter("carregarImagemUrl")
+fun ImageView.carregarImagemUrl(url: String) {
+    carregaImagem(url)
+}
